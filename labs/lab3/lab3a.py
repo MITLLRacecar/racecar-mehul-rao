0411cbe896f3a8b8565ebfd63be5ddec0ca650b7
Mehul Rao <mehulrao@gmail.com>
2021-07-12 15:35:49 -0400

    lab 3a and 3b

--- labs/lab3/lab3a.py
+++ labs/lab3/lab3a.py
@@ -13,7 +13,8 @@ Lab 3A - Depth Camera Safety Stop
 import sys
 import cv2 as cv
 import numpy as np
-
+from typing import Any, Tuple, List, Optional
+from nptyping import NDArray
 sys.path.insert(0, "../../library")
 import racecar_core
 import racecar_utils as rc_utils
@@ -30,6 +31,38 @@ rc = racecar_core.create_racecar()
 # Functions
 ########################################################################################
 
+def get_closest_pixel(
+    depth_image: NDArray[(Any, Any), np.float32],
+    kernel_size: int = 5
+) -> Tuple[int, int]:
+    """
+    Finds the closest pixel in a depth image.
+
+    Args:
+        depth_image: The depth image to process.
+        kernel_size: The size of the area to average around each pixel.
+
+    Returns:
+        The (row, column) of the pixel which is closest to the car.
+
+    Warning:
+        kernel_size be positive and odd.
+        It is highly recommended that you crop off the bottom of the image, or else
+        this function will likely return the ground directly in front of the car.
+
+    Note:
+        The larger the kernel_size, the more that the depth of each pixel is averaged
+        with the distances of the surrounding pixels.  This helps reduce noise at the
+        cost of reduced accuracy.
+    """
+    # Shift 0.0 values to 10,000 so they are not considered for the closest pixel
+    depth_image = (depth_image - 0.01) % 10000
+
+    kernel_size = 11
+    blurred_image = cv.GaussianBlur(depth_image, (kernel_size, kernel_size), 0)
+    minVal, maxVal, minLoc, maxLoc = cv.minMaxLoc(blurred_image)
+
+    return np.flip(minLoc)
 
 def start():
     """
@@ -64,10 +97,21 @@ def update():
 
     # Calculate the distance of the object directly in front of the car
     depth_image = rc.camera.get_depth_image()
-    center_distance = rc_utils.get_depth_image_center_distance(depth_image)
+    depth_image = (depth_image - 0.01) % 10000
 
     # TODO (warmup): Prevent forward movement if the car is about to hit something.
     # Allow the user to override safety stop by holding the right bumper.
+
+    # Apply a gaussian blur
+    kernel_size = 11
+    blurred_image = cv.GaussianBlur(depth_image, (kernel_size, kernel_size), 0)
+
+    # Retrieve the distance of the central pixel
+    center_distance = rc_utils.get_depth_image_center_distance(blurred_image)
+
+    if center_distance < 45 and not rc.controller.is_down(rc.controller.Button.RB):
+        print("STOP INITIALIZED")
+        speed = -1
 
     # Use the left joystick to control the angle of the front wheels
     angle = rc.controller.get_joystick(rc.controller.Joystick.LEFT)[0]
@@ -83,10 +127,11 @@ def update():
         print("Center distance:", center_distance)
 
     # Display the current depth image
-    rc.display.show_depth_image(depth_image)
+    rc.display.show_depth_image(blurred_image)
 
     # TODO (stretch goal): Prevent forward movement if the car is about to drive off a
     # ledge.  ONLY TEST THIS IN THE SIMULATION, DO NOT TEST THIS WITH A REAL CAR.
+
 
     # TODO (stretch goal): Tune safety stop so that the car is still able to drive up
     # and down gentle ramps.
--- labs/lab3/lab3bOld.py
+++ labs/lab3/lab3bOld.py
@@ -23,7 +23,6 @@ import racecar_utils as rc_utils
 ########################################################################################
 # Global variables
 ########################################################################################
-# Sets up the racecar object
 rc = racecar_core.create_racecar()
 
 # >> Constants
@@ -40,13 +39,15 @@ contour_center = None  # The (pixel row, pixel column) of contour
 contour_area = 0  # The area of contour
 
 # Add any global variables here
-isParked = False # Set to true once the car has stopped around 30cm in front of the cone
+
+isParked = False
 
 ########################################################################################
 # Functions
 ########################################################################################
 def get_closest_pixel(
-    image: NDArray[(Any, Any), np.float32],
+    depth_image: NDArray[(Any, Any), np.float32],
+    kernel_size: int = 5
 ) -> Tuple[int, int]:
     """
     Finds the closest pixel in a depth image.
@@ -68,9 +69,14 @@ def get_closest_pixel(
         with the distances of the surrounding pixels.  This helps reduce noise at the
         cost of reduced accuracy.
     """
-    minVal, maxVal, minLoc, maxLoc = cv.minMaxLoc(image)
+    # Shift 0.0 values to 10,000 so they are not considered for the closest pixel
+    depth_image = (depth_image - 0.01) % 10000
 
-    return minLoc
+    kernel_size = 11
+    blurred_image = cv.GaussianBlur(depth_image, (kernel_size, kernel_size), 0)
+    minVal, maxVal, minLoc, maxLoc = cv.minMaxLoc(blurred_image)
+
+    return np.flip(minLoc)
 
 def remap_range(
     val: float,
@@ -135,31 +141,6 @@ def update_contour():
             contour_center = None
             contour_area = 0
 
-def get_mask(
-    image: NDArray[(Any, Any, 3), np.uint8],
-    hsv_lower: Tuple[int, int, int],
-    hsv_upper: Tuple[int, int, int]
-) -> NDArray[Any, Any]:
-    """
-    Returns a mask containing all of the areas of image which were between hsv_lower and hsv_upper.
-
-    Args:
-        image: The image (stored in BGR) from which to create a mask.
-        hsv_lower: The lower bound of HSV values to include in the mask.
-        hsv_upper: The upper bound of HSV values to include in the mask.
-    """
-    # Convert hsv_lower and hsv_upper to numpy arrays so they can be used by OpenCV
-    hsv_lower = np.array(hsv_lower)
-    hsv_upper = np.array(hsv_upper)
-
-    # TODO: Use the cv.cvtColor function to switch our BGR colors to HSV colors
-    image = cv.cvtColor(image, cv.COLOR_BGR2HSV)
-
-    # TODO: Use the cv.inRange function to highlight areas in the correct range
-    mask = cv.inRange(image, hsv_lower, hsv_upper)
-
-    return mask
-
 def start():
     """
     This function is run once every time the start button is pressed
@@ -214,66 +195,46 @@ def update():
     update_contour()
 
     # TODO: Park the car 30 cm away from the closest orange cone
-    # Tries to turn at the angle corresponding to the contour center
-    # If no contour center, then turn at an angle of 0.3
     try:
         angle = remap_range(contour_center[1], 0, rc.camera.get_width(), -1, 1)
     except:
         print("Angle Error: setting angle to 0.3")
         angle = 0.3
 
-    # Gets a depth image, and filters for noise and null values
     depth_image = rc.camera.get_depth_image()
+    depth_image = (depth_image - 0.01) % 10000
     kernel_size = 11
     blurred_image = cv.GaussianBlur(depth_image, (kernel_size, kernel_size), 0)
-
-    # Crops the depth image
     top_left_inclusive = (0, 0)
     bottom_right_exclusive = ((rc.camera.get_height() // 3) * 2, rc.camera.get_width())
     cropped_image = crop(blurred_image, top_left_inclusive, bottom_right_exclusive)
-
-
-	# Using the color camera to mask the depth camera
-    image = rc.camera.get_color_image()
-    mask = get_mask(image, ORANGE[0], ORANGE[1])
-    masked_depth_image = cv.bitwise_and(depth_image, depth_image, mask=mask)
-    masked_depth_image = (masked_depth_image - 0.01) % 10000
-    rc.display.show_depth_image(masked_depth_image)
-
-    # Gets the distance to the closest value in the new masked depth image
-    x, y = get_closest_pixel(masked_depth_image)
-    distance = masked_depth_image[y, x]
+    rc.display.show_depth_image(cropped_image)
+    y, x = get_closest_pixel(cropped_image)
+    distance = cropped_image[y][x]
     print(distance)
 
-    # Checks if car overshot parking and backs up
     if distance < 29:
         angle *= -1
         speed = remap_range(distance, 0, 30, -1, 0)
         speed = np.clip(speed, -1, 1)
         isParked = False
         print("Backing up")
-
-    # checks if the car is parked
-    elif distance < 30:
+    elif distance < 33.3:
         rc.drive.stop()
         speed = 0
         angle = 0
         isParked = True
         print("parked")
-
     else:
         if not isParked:
-            # Tries to set speed at the value corresponding to the contour center
-            # If no contour center, then set speed to 0.2
             try:
                 speed = remap_range(distance, 30, 500, 0, 1)
                 speed = np.clip(speed, -1, 1)
             except:
                 print("Speed Error: setting speed to 0.2")
                 speed = 0.2
-
-    # Set the speed and angle of the car according to all our calculatings!
     print(speed)
+
     rc.drive.set_speed_angle(speed, angle)
 
     # Print the current speed and angle when the A button is held down
